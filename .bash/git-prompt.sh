b27f36e3e725b1c535acc9a88cfd496b7c7099bf
Eric Crooks <eric.crooks@gmail.com>
2019-06-09 18:34:07 -0400

    initial commit

---
+++ .bash/git-prompt.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Insert Git branch into prompt
+
+##
+# Parse the Git branch name if it exists. That is, we're in a Git directory.
+#
+__crookse_parse_git_branch() {
+  if [[ "${1}" == "clean-no-branch-category" ]]; then
+    command git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/' -e 's/.*\///'
+    return
+  fi
+  if [[ "${1}" == "clean" ]]; then
+    command git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'
+    return
+  fi
+  command git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
+}
+
+##
+# Check if the current directory is a Git repository
+#
+__crookse_check_if_git() {
+	if [[ $(__crookse_parse_git_branch) ]]; then
+		local branch=$(__crookse_parse_git_branch)
+		# 45, 232 (black on light blue)
+		# 166 orange
+		tput setaf 166
+		echo -en "\ngit: ${branch}"
+		tput sgr0
+	fi
+}
---
+++ hidden/README.md
@@ -0,0 +1,3 @@
+# Hidden Directory
+
+Place anything in here that you want hidden from your repository. Anything placed in this directory will be ignored by the .gitignore file.
